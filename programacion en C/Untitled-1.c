c3139e7c0151ca6719027d4313dd127155b1b04f
saasiru3 <saasiru3>
2025-02-06 14:14:42 +0100

    Ejercicio1InteractivasPromedioSumatorioParesImpares

---
+++ programacion en C/Untitled-1.c
@@ -0,0 +1,14 @@
+#include<stdio>
+int main () {
+    /*funcion para generar la serie Fibonacci*/
+    int a=0, b=1,siguente;
+    /*mostrar los primeros dos terminos de la serie*/
+    printf ("%d", a);
+    if (n>1) {
+        printf ("%d)"b;)
+    }
+    /*generar los siguientes terminos*/
+    for (int i=)
+
+
+}
